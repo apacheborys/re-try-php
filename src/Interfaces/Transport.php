98f2cdf59b57104924274cb39647fa0cdb12de6b
Borys Yermokhin <borys.ermokhin@gmail.com>
2022-12-17 18:58:37 +0100

    fix interface

--- src/Interfaces/Transport.php
+++ src/Interfaces/Transport.php
@@ -4,17 +4,18 @@ declare(strict_types=1);
 namespace ApacheBorys\Retry\Interfaces;
 
 use ApacheBorys\Retry\Entity\{Config, Message};
+use Generator;
 
 interface Transport
 {
     public function send(Message $message): bool;
 
-    /** @return array<int, Message>|null */
+    /** @return Message[]|Generator<int, Message>|null */
     public function fetchUnprocessedMessages(int $batchSize = -1): ?iterable;
 
     public function getNextId(\Throwable $exception, Config $config): string;
 
-    /** @return array<int, Message> */
+    /** @return Message[]|Generator<int, Message> */
     public function getMessages(int $limit = 100, int $offset = 0, bool $byStream = false): iterable;
 
     public function howManyTriesWasBefore(\Throwable $exception, Config $config): int;
